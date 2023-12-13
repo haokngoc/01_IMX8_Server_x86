4e8f4d9aab1773a59cbcdb369a88b26344a68f64
haokngoc <haooila7@gmail.com>
2023-12-13 15:12:20 +0700

    update thread v.1

--- src/MsgHandler.cpp
+++ src/MsgHandler.cpp
@@ -1,7 +1,7 @@
-// MsgHandler.cpp
 #include "MsgHandler.h"
 #include <iostream>
 #include <stdexcept>
+#include <thread>
 
 #define MARKER_HEAD 0xAA
 #define MARKER_TAIL 0x55
@@ -12,6 +12,14 @@ MsgHandler::~MsgHandler() {
     delete currentState;
 }
 
+void* connectionHandler(void* arg) {
+    MsgHandler* msgHandler = static_cast<MsgHandler*>(arg);
+
+    // Gọi đối tượng MsgHandler để xử lý gói tin từ client
+    msgHandler->parseMsgClient(msgHandler->getCurrentSocket());
+
+    pthread_exit(nullptr);
+}
 void MsgHandler::handleConnections() {
     while (true) {
         sockpp::inet_address peer;
@@ -21,21 +29,30 @@ void MsgHandler::handleConnections() {
             std::cerr << "Error accepting incoming connection: " << acceptor_.last_error_str() << std::endl;
             continue;
         }
-
         std::cout << "Received a connection request from " << peer << std::endl;
+//        // Tạo một luồng mới để xử lý gói tin từ client
+//		std::thread([&]() {
+//			// Gọi đối tượng MsgHandler để xử lý gói tin từ client
+//			parseMsgClient(currentSocket);
+//			currentSocket.close();
+//			std::cout << "Connection closed from " << peer << std::endl;
+//		}).detach();
+//		//parseMsgClient(currentSocket);
 
-        // Gọi đối tượng MsgHandler để xử lý gói tin từ client
-        parseMsgClient(currentSocket);
+        pthread_t thread;
+		int result = pthread_create(&thread, nullptr, connectionHandler, this);
+		if (result != 0) {
+			std::cerr << "Error creating thread: " << strerror(result) << std::endl;
 
-        // Đóng kết nối với client
-//        currentSocket.close();
-//        std::cout << "Connection closed from " << peer << std::endl;
+		}
+		pthread_detach(thread);
     }
 }
-
+sockpp::tcp_socket& MsgHandler::getCurrentSocket(){
+	return currentSocket;
+}
 
 void MsgHandler::parseMsgClient(sockpp::tcp_socket& socket) {
-    int ret = -1;
 
     try {
         // Đọc dữ liệu từ socket
@@ -60,7 +77,6 @@ void MsgHandler::parseMsgClient(sockpp::tcp_socket& socket) {
                 transitionToState(new WorkState());
                 break;
             default:
-                ret = -1;
                 break;
         }
     } catch (const std::exception& e) {
@@ -81,8 +97,7 @@ void MsgHandler::sendDataToClient(const std::vector<char>& data) {
         int n = currentSocket.write(data.data(), data.size());
         if(n<0) {
         	std::cout << "Loi!!!" << std::endl;
-        }
-        else {
+        } else {
         	std::cout << "Sent data to Client" << std::endl;
         }
     } catch (const std::exception& e) {
